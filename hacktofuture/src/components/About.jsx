a29e4a83df3e6cc65171cdbcca7d39d9b8b08ace
MacDsouza <120902232+MacDsouza@users.noreply.github.com>
2024-01-17 23:53:43 +0530

    created the about component

--- hacktofuture/src/components/About.jsx
+++ hacktofuture/src/components/About.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const About = () => {
+const About = ({ title, info, Vission, Mission, Img }) => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="bg-gradient-to-r from-neutral-900 to-zinc-900 w-1/2 h-auto m-3 p-3 text-white text-center  rounded-lg ">
+      <img src={Img} alt="Logo" className="mx-auto w-48 h-48" />
+      <p className="text-6xl font-light ">About {title}</p>
+      <p className="text-xl font-extralight">{info}</p>
+      <br />
+      {Vission && <p className="text-xl font-extralight"><b>Vission</b>-{Vission}</p>}
+      {Mission && <p className="text-xl font-extralight"><b>Mission</b>-{Mission}</p>}
+    </div>
+  );
+};
 
-export default About
+export default About;
