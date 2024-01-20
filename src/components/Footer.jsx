de74bb90c63d6be76cc3bc534ea663c2ace214aa
septicalls <septicalls@proton.me>
2024-01-20 22:51:11 +0530

    updated footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,13 @@ const Footer = () => {
   return (
     <footer className="bg-bg_color_2 py-2 w-full">
       <div className="flex items-center justify-between h-12">
-        <p className="text-text_col_1 font-semibold text-2xl ml-4 sm:ml-8 ">
-          SJEC
-        </p>
+        <a href="https://sjec.ac.in/">
+          <p className="text-text_col_1 font-semibold text-2xl ml-4 sm:ml-8 ">
+            SJEC
+          </p>
+        </a>
         <p className="text-text_col_1 font-thin text-xs  w-56 bottom-11 sm:bottom-3">
-          &copy;2024 the sceptix club | All rights reserved.
+          &copy;2024 The sceptix club | All rights reserved.
         </p>
         <img
           src="src/assets/htf-logo.png"
