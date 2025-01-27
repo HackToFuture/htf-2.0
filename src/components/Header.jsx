604d7ef3233ca5ba82311bcf44206e7b4d22d3db
pranavvraja <pranavvraja@gmail.com>
2025-01-27 23:22:20 +0530

    update: scroll banner coming soon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 import htflogo from "../assets/hero-htf-main.svg";
 import RegModal from "./RegModal/RegModal";
+import ScrollingBanner from "./ScrollBanner";
 
 const Header = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
@@ -35,176 +36,178 @@ const Header = () => {
   };
 
   return (
-    <header
-      className={`
-      bg-bg_color bg-opacity-40 backdrop-blur-blur1 h-16 md:h-20 flex items-center fixed top-0 w-full z-10 transition-transform duration-500 ${
-        visible ? "transform translate-y-0" : "transform -translate-y-20"
+    <>
+      <ScrollingBanner />
+      <header
+        className={`
+      bg-bg_color bg-opacity-40 backdrop-blur-blur1 h-16 md:h-20 flex items-center fixed top-10 w-full z-10 transition-transform duration-500 ${
+        visible ? "transform translate-y-0" : "transform -translate-y-32"
       }
       `}
-    >
-      {isMobile ? (
-        <nav className="flex-grow flex justify-between items-center mr-6">
-          <div className="flex-shrink-0 ml-8">
-            <img src={htflogo} alt="logo" className="aspect-auto w-24" />
-          </div>
+      >
+        {isMobile ? (
+          <nav className="flex-grow flex justify-between items-center mr-6">
+            <div className="flex-shrink-0 ml-8">
+              <img src={htflogo} alt="logo" className="aspect-auto w-24" />
+            </div>
 
-          <button
-            onClick={handleClick}
-            className="flex flex-col justify-center items-center mr-3"
-          >
-            <span
-              className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
-                isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
-              }`}
-            ></span>
-            <span
-              className={`bg-white block transition-all duration-300 ease-out 
+            <button
+              onClick={handleClick}
+              className="flex flex-col justify-center items-center mr-3"
+            >
+              <span
+                className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+                  isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+                }`}
+              ></span>
+              <span
+                className={`bg-white block transition-all duration-300 ease-out 
                     h-0.5 w-6 rounded-sm my-0.5 ${
                       isOpen ? "opacity-0" : "opacity-100"
                     }`}
-            ></span>
-            <span
-              className={`bg-white block transition-all duration-300 ease-out 
+              ></span>
+              <span
+                className={`bg-white block transition-all duration-300 ease-out 
                     h-0.5 w-6 rounded-sm ${
                       isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
                     }`}
-            ></span>
-          </button>
-        </nav>
-      ) : (
-        <nav className="flex-grow flex justify-between items-center ">
-          <div className="flex-shrink-0 ml-8 cursor-pointer">
-            <Link
-              activeClass="active"
-              to="hero"
-              spy={true}
-              smooth={true}
-              offset={-30}
-              duration={500}
-            >
-              <img src={htflogo} alt="logo" className="aspect-auto w-24" />
-            </Link>
-          </div>
-          <ul className="flex justify-center font-urbanist font-medium text-md">
-            <li className="text-white hover:text-text_col_2 transition px-8 cursor-pointer">
+              ></span>
+            </button>
+          </nav>
+        ) : (
+          <nav className="flex-grow flex justify-between items-center ">
+            <div className="flex-shrink-0 ml-8 cursor-pointer">
               <Link
                 activeClass="active"
-                to="about"
+                to="hero"
                 spy={true}
                 smooth={true}
                 offset={-30}
                 duration={500}
               >
-                About
+                <img src={htflogo} alt="logo" className="aspect-auto w-24" />
               </Link>
-            </li>
-            <li className="text-white hover:text-text_col_2 transition px-8 cursor-pointer">
-              <Link
-                activeClass="active"
-                to="prize"
-                spy={true}
-                smooth={true}
-                offset={-30}
-                duration={500}
-              >
-                Prizes
-              </Link>
-            </li>
-            <li className="text-white hover:text-text_col_2 transition px-8 cursor-pointer">
-              <Link
-                activeClass="active"
-                to="theme"
-                spy={true}
-                smooth={true}
-                offset={-30}
-                duration={500}
-              >
-                Themes
-              </Link>
-            </li>
+            </div>
+            <ul className="flex justify-center font-urbanist font-medium text-md">
+              <li className="text-white hover:text-text_col_2 transition px-8 cursor-pointer">
+                <Link
+                  activeClass="active"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-30}
+                  duration={500}
+                >
+                  About
+                </Link>
+              </li>
+              <li className="text-white hover:text-text_col_2 transition px-8 cursor-pointer">
+                <Link
+                  activeClass="active"
+                  to="prize"
+                  spy={true}
+                  smooth={true}
+                  offset={-30}
+                  duration={500}
+                >
+                  Prizes
+                </Link>
+              </li>
+              <li className="text-white hover:text-text_col_2 transition px-8 cursor-pointer">
+                <Link
+                  activeClass="active"
+                  to="theme"
+                  spy={true}
+                  smooth={true}
+                  offset={-30}
+                  duration={500}
+                >
+                  Themes
+                </Link>
+              </li>
 
-            <li className="text-white hover:text-text_col_2 transition px-8 cursor-pointer">
-              <Link
-                activeClass="active"
-                to="faq"
-                spy={true}
-                smooth={true}
-                offset={-30}
-                duration={500}
-              >
-                FAQ
-              </Link>
-            </li>
-          </ul>
+              <li className="text-white hover:text-text_col_2 transition px-8 cursor-pointer">
+                <Link
+                  activeClass="active"
+                  to="faq"
+                  spy={true}
+                  smooth={true}
+                  offset={-30}
+                  duration={500}
+                >
+                  FAQ
+                </Link>
+              </li>
+            </ul>
 
-          {/* <button
+            {/* <button
             onClick={googleAuth}
             className="text-white text-lg font-bold  px-8 py-1 rounded-3xl mr-6"
             id="register"
           >
             <a href="/registration">Register</a>
           </button> */}
-          <RegModal />
-        </nav>
-      )}
-      {isOpen && (
-        <div
-          className="fixed top-20 right-2 h-auto w-48 py-4 backdrop-blur-blur1  bg-bg_color_2 bg-opacity-70 rounded-xl shadow-xl flex flex-col items-center justify-center bg-blend-overlay"
-          id="hammenu"
-        >
-          <ul className="flex flex-col text-lg items-center font-urbanist font-medium text-text_col_1 cursor-pointer">
-            <li className="transition px-8 hover:text-black">
-              <Link
-                activeClass="active"
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-30}
-                duration={500}
-              >
-                About
-              </Link>
-            </li>
-            <li className="transition px-8 hover:text-black">
-              <Link
-                activeClass="active"
-                to="prize"
-                spy={true}
-                smooth={true}
-                offset={-30}
-                duration={500}
-              >
-                Prizes
-              </Link>
-            </li>
-            <li className="transition px-8 hover:text-black">
-              <Link
-                activeClass="active"
-                to="theme"
-                spy={true}
-                smooth={true}
-                offset={-30}
-                duration={500}
-              >
-                Themes
-              </Link>
-            </li>
-            <li className="transition px-8 hover:text-black">
-              <Link
-                activeClass="active"
-                to="faq"
-                spy={true}
-                smooth={true}
-                offset={-30}
-                duration={500}
-              >
-                FAQ
-              </Link>
-            </li>
-          </ul>
-          <div className=" h-16 flex items-center justify-center">
-            <div className="m-5">
-              {/* <button
+            <RegModal />
+          </nav>
+        )}
+        {isOpen && (
+          <div
+            className="fixed top-20 right-2 h-auto w-48 py-4 backdrop-blur-blur1  bg-bg_color_2 bg-opacity-70 rounded-xl shadow-xl flex flex-col items-center justify-center bg-blend-overlay"
+            id="hammenu"
+          >
+            <ul className="flex flex-col text-lg items-center font-urbanist font-medium text-text_col_1 cursor-pointer">
+              <li className="transition px-8 hover:text-black">
+                <Link
+                  activeClass="active"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-30}
+                  duration={500}
+                >
+                  About
+                </Link>
+              </li>
+              <li className="transition px-8 hover:text-black">
+                <Link
+                  activeClass="active"
+                  to="prize"
+                  spy={true}
+                  smooth={true}
+                  offset={-30}
+                  duration={500}
+                >
+                  Prizes
+                </Link>
+              </li>
+              <li className="transition px-8 hover:text-black">
+                <Link
+                  activeClass="active"
+                  to="theme"
+                  spy={true}
+                  smooth={true}
+                  offset={-30}
+                  duration={500}
+                >
+                  Themes
+                </Link>
+              </li>
+              <li className="transition px-8 hover:text-black">
+                <Link
+                  activeClass="active"
+                  to="faq"
+                  spy={true}
+                  smooth={true}
+                  offset={-30}
+                  duration={500}
+                >
+                  FAQ
+                </Link>
+              </li>
+            </ul>
+            <div className=" h-16 flex items-center justify-center">
+              <div className="m-5">
+                {/* <button
                 onClick={(e) => googleAuth(e)}
                 className="bg-bg_color_2 transition duration-300 ease-in-out text-lg font-bold px-10 py-1 hover:scale-105 rounded-3xl mr-6 "
               >
@@ -212,12 +215,13 @@ const Header = () => {
                   Register
                 </a>
               </button> */}
-              <RegModal />
+                <RegModal />
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </header>
+        )}
+      </header>
+    </>
   );
 };
 
---
+++ src/components/ScrollBanner.jsx
@@ -0,0 +1,29 @@
+import { motion } from "framer-motion";
+
+export default function ScrollingBanner() {
+  const sentence = "   The 3rd edition coming soon. Stay Tuned!   •   ";
+  const repeatCount = 15; // Increase this number to add more repetitions
+
+  return (
+    <div className="bg-blue-500 h-fit fixed top-0 text-white overflow-hidden py-2 z-50">
+      <motion.div
+        className="whitespace-nowrap inline-block"
+        animate={{
+          x: ["0%", "-50%"],
+        }}
+        transition={{
+          x: {
+            repeat: Infinity,
+            repeatType: "loop",
+            duration: 15, // Reduced from 20 to 15 to speed up the animation
+            ease: "linear",
+          },
+        }}
+      >
+        <span className="text-lg font-semibold px-4">
+          {sentence.repeat(repeatCount)}
+        </span>
+      </motion.div>
+    </div>
+  );
+}
